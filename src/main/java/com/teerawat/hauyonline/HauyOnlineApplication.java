20be3dd13fd8135df52cf69f830bcf458dedd69d
teerawat.yamsai <teerawat.yamsai@krungsri.com>
2019-12-21 00:06:34 +0700

    init porject

---
+++ src/main/java/com/teerawat/hauyonline/HauyOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.teerawat.hauyonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HauyOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HauyOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teerawat/hauyonline/HauyOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teerawat.hauyonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HauyOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
